9584e9d3c53ac83412f520e6b6b110bc6b2af34e
Тимофей Никитан <timofejnikitan@Mac.lan>
2025-09-11 21:55:10 +0300

    44.4 Quartz

---
+++ src/main/java/ru/quartz/AlertRabbit.java
@@ -0,0 +1,55 @@
+package ru.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Properties properties = loadProperties("src/main/resources/rabbit.properties");
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            int interval = Integer.parseInt(properties.getProperty("rabbit.interval"));
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(interval)
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+
+    private static Properties loadProperties(String fileName) {
+        Properties properties = new Properties();
+        try (FileInputStream in = new FileInputStream(fileName)) {
+            properties.load(in);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return properties;
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+}
